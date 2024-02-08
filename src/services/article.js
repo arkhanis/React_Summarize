1c8e2a3f25274686feb63c38f12f56b53a33d8bb
arkhanis <casanovac@gmail.com>
2024-02-08 14:58:17 -0300

    redux

--- src/services/article.js
+++ src/services/article.js
@@ -1,27 +1,25 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-const RapidApiKey = import.meta.env.VITE_RAPID_API_KEY;
-
+const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
 
 export const articleApi = createApi({
-    
     reducerPath: 'articleApi',
-
-    /* From RapidAPI url method GET */
-    baseQuery: fetchBaseQuery({ 
-        baseUrl: 'https://article-extractor-and-summarizer.p.rapidapi.com' }),
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://article-extractor-and-summarizer.p.rapidapi.com/',
         prepareHeaders: (headers) => {
-            headers.set('X-RapidAPI-Key', 'RapidApiKey');
+            headers.set('X-RapidAPI-Key', rapidApiKey);
             headers.set('X-RapidAPI-Host', 'article-extractor-and-summarizer.p.rapidapi.com');
+
             return headers;
         },
-
+    }),
     endpoints: (builder) => ({
-            getSummary: builder.query({
-            query: (params) => `/summarize?url=${encodeURIComponent(params.articleUrl)}&lenght=3`
-        })
-    })
+        getSummary: builder.query({
+            // encodeURIComponent() function encodes special characters that may be present in the parameter values
+            // If we do not properly encode these characters, they can be misinterpreted by the server and cause errors or unexpected behavior. Thus that RTK bug
+            query: (params) => `summarize?url=${encodeURIComponent(params.articleUrl)}&length=3`,
+        }),
+    }),
+})
 
-    });
-
-export const { useLazyGetSummaryQuery } = articleApi;
+export const { useLazyGetSummaryQuery } = articleApi
