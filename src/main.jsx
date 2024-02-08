409da1af2458c2103dd8ca2aa1aad7950760e795
arkhanis <casanovac@gmail.com>
2024-02-08 14:54:08 -0300

    redux

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-   
-  <App />
+import { Provider } from 'react-redux';
+import { store } from './services/store';
 
-  )
+ReactDOM.createRoot(document.getElementById('root')).render(
+
+  <Provider store={store} >
+    <App />
+  </Provider>
+
+)
