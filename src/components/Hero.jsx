457841776e4ebc15f28ca1ece97df1e06bea0d62
arkhanis <casanovac@gmail.com>
2024-02-08 13:12:19 -0300

    set up

---
+++ src/components/Hero.jsx
@@ -0,0 +1,19 @@
+import { logo } from '../assets';
+
+const Hero = () => {
+  return (
+    <header className='w-full flex justifify-center items-center flex-col'>
+        <nav className='flex justify-between items-center flex-col'>
+            <img src={logo} alt="sumz_logo" className='w-28 object-contain'/>
+            <button
+                type="button"
+                onClick={() => window.open('https://github.com/arkhanis/React_Summarize')}
+            >
+                GitHub
+            </button>
+        </nav>
+    </header>
+  )
+}
+
+export default Hero
